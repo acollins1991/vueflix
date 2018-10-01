2290de65097882e1b66db05bbd31c13167642f2e
Anthony Collins <anthony@themarketingcampaigncompany.co.uk>
2018-10-01 21:56:34 +0100

    0ece98e

---
+++ precache-manifest.132865d89f03e4de20d487201363a1d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueflix/robots.txt"
+  },
+  {
+    "revision": "cd0f7feeb83ec49c35dc",
+    "url": "/vueflix/js/chunk-vendors.6d56fb31.js"
+  },
+  {
+    "revision": "3e2a88960713c2a994de",
+    "url": "/vueflix/js/app.74da2c14.js"
+  },
+  {
+    "revision": "6249ec0a09fa811f3d25",
+    "url": "/vueflix/js/about.2ca2c031.js"
+  },
+  {
+    "revision": "c5b890e0ce9f8e138cf8798aa628f2c8",
+    "url": "/vueflix/index.html"
+  },
+  {
+    "revision": "bb45549239e25f1770d5f76727bcd7c0",
+    "url": "/vueflix/img/powered-by-tmdb.bb455492.png"
+  },
+  {
+    "revision": "cd0f7feeb83ec49c35dc",
+    "url": "/vueflix/css/chunk-vendors.d67dc552.css"
+  },
+  {
+    "revision": "3e2a88960713c2a994de",
+    "url": "/vueflix/css/app.16e3d515.css"
+  }
+];
